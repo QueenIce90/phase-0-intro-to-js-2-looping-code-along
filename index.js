18ab2e833239cda1b935fecfff1562e150478e19
QueenIce90 <Isamargonzalez90@gmail.com>
2023-09-28 21:07:12 -0400

    short message briefly explaining the changes the changes saved in this commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+// index.js
+
+// Function to write thank you messages
+function writeCards(names) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // Function to count down from a number
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Export the functions if needed
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
